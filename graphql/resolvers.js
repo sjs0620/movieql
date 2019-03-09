b1fae7a6a0c90e58cd9cfcd445f001c7191c9013
sjs0620 <sjslsm@gmail.com>
2019-03-10 00:40:42 +0900

    #5

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,16 @@
+const nicolas = {
+  name: "Nicolas",
+  age: 18,
+  gender: "female"
+}
+
 const resolvers = {
   Query: {
-    name: () => "nicolas"
+    person: () => nicolas
   }
 };
+
+// 화살표함수 () => nicolas
+// function() {return nicolas}와 같음
 
 export default resolvers;
